0e8b46366ab36bff56dbf8082bdbdd771d195a0d
novd7 <103358170+novd7@users.noreply.github.com>
2022-10-24 18:17:27 +0300

    some bugs fixed
    other tasks work!!!! :)
    Thank you, Nikita Maksimovich

--- main.py
+++ main.py
@@ -24,10 +24,11 @@ if __name__ == '__main__':
     print(data)
     for r, w in data[:count]:
         print(r)
-        app2 = QApplication(sys.argv)
+        # app2 = QApplication(sys.argv)
         ex2 = MainWindowTask1(r)
         ex2.show()
-        app2.exec_()
+        app.exec_()
+        # app.exec()
         res = ex2.wsw.res
         if res == "cons":
             # TODO: open new window with funny image "Go to the first form!:)"
